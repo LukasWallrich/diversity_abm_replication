61b8af0ba9badd6e59245ab4d838d8f7904a7ce5
Lukas Wallrich <lukas.wallrich@gmail.com>
2022-09-09 16:30:58 +0100

    Complete model formatting and docstrings

--- Grim_et_al/Gmodel.py
+++ Grim_et_al/Gmodel.py
@@ -1,9 +1,10 @@
 # Used this import of HPModel to avoid duplicating files
 import httpimport
+
 url = "https://gist.githubusercontent.com/LukasWallrich/05f445821fbae694b37a205dc08b2b4f/raw/"
 
 with httpimport.remote_repo(["HPmodel"], url):
-     from HPmodel import HPProblem, PSAgent
+    from HPmodel import HPProblem, PSAgent
 
 # Alternative: download file into same folder, then run
 # from HPmodel import HPProblem, PSAgent
@@ -11,6 +12,7 @@ with httpimport.remote_repo(["HPmodel"], url):
 import pandas as pd
 import numpy as np
 from copy import copy
+
 
 class GrimAgent(PSAgent):
     """Agent for Hong-Page problem-solving model as extended by Grim et al.
@@ -25,11 +27,13 @@ class GrimAgent(PSAgent):
     """
 
     def step(self):
-        """Search for highest peak accessible with own heuristic, in tournament or relay mode (called by mesa)"""        
+        """Search for highest peak accessible with own heuristic, in tournament or relay mode (called by mesa)"""
         if self.problem.strategy == "relay":
-            self.focus, self.best_solution = self.problem.max_search(agent = self)
+            self.focus, self.best_solution = self.problem.max_search(agent=self)
         if self.problem.strategy == "tournament":
-            self.focus, self.best_solution = self.problem.max_search(agent = self, update = False)
+            self.focus, self.best_solution = self.problem.max_search(
+                agent=self, update=False
+            )
 
 
 class GProblem(HPProblem):
@@ -37,8 +41,8 @@ class GProblem(HPProblem):
     """Hong & Page problem-solving model, extended by Grim et al, to assess performance of different teams.
 
     This mesa-based agent-based model implements the model described by Grim et al. (2019),
-    which extends the original model by Hong & Page (2004) to account for landscapes of varying smoothness 
-    and to contrast the original 'relay' model with a 'tournament' model. 
+    which extends the original model by Hong & Page (2004) to account for landscapes of varying smoothness
+    and to contrast the original 'relay' model with a 'tournament' model.
 
     Attributes:
         agent_descriptives: Dict with descriptive statistics for agents in each team (i.e. random and best).
@@ -46,13 +50,23 @@ class GProblem(HPProblem):
 
     Methods:
         max_search: Evaluate a heuristic across all starting points, or have an agent search from their current location.
-        step: Advance model by one step.    
+        step: Advance model by one step.
     """
 
-    def __init__(self, n: int, k: int, l: int, N_agents: int, smoothness: int, seed:int = None, strategy: str = "relay", agent_class: PSAgent = GrimAgent):
+    def __init__(
+        self,
+        n: int,
+        k: int,
+        l: int,
+        N_agents: int,
+        smoothness: int,
+        seed: int = None,
+        strategy: str = "relay",
+        agent_class: PSAgent = GrimAgent,
+    ):
         """Initializes problem, assesses heuristics and creates agent teams
-        
-        Initializes Hong & Page model as extended by Grim et al by drawing a smoothed solution landscape, 
+
+        Initializes Hong & Page model as extended by Grim et al by drawing a smoothed solution landscape,
         assessing the performance of all heuristics, and creating best and random agent teams.
 
         Args:
@@ -62,84 +76,111 @@ class GProblem(HPProblem):
             N_agents: Number of agents in each team
             smoothness: Smoothness of solution landscape, i.e. the average distance between randomly selected points between which the solution is interpolated.
             seed: Random seed for reproducibility
-            strategy: 
+            strategy:
               Problem solving strategy for teams. Can be 'relay', 'tournament' or 'both. In 'relay' mode, agents sequentially search for improvements,
               if they find one, their entire team moves to their improved solution and the next agent continues from there. In 'tournament' mode, each
               agent independently searches for improvements, and then the teams move to the best solution found in that round.
-    
-        """        
-        self.draw_G_solution(n, smoothness)
-        super().__init__(n, k, l, N_agents, seed, agent_class = agent_class)
+
+        """
+        self.__draw_G_solution(n, smoothness)
+        super().__init__(n, k, l, N_agents, seed, agent_class=agent_class)
         self.strategy = strategy
 
-    def draw_solution(self, n):
-        """Overridden in favor of draw_G_solution"""        
+    def __draw_solution(self, n):
+        """Overridden in favor of draw_G_solution"""
         pass
 
-    def draw_G_solution(self, n: int, smoothness: int) -> None:
-        """Generate solution landscape of length n, consisting of random values approximately smoothness apart and interpolated"""        
-        if (smoothness == 0):
-            super().draw_solution(n)
-        else:    
+    def __draw_G_solution(self, n: int, smoothness: int) -> None:
+        """Generate solution landscape of length n, consisting of random values approximately smoothness apart and interpolated"""
+        if smoothness == 0:
+            self._HPProblem__draw_solution(n)
+        else:
             i = 0
             solution = pd.Series(np.nan for x in range(n))
-            while(i < n): # Create random heights on average every `smoothness` steps apart
+            while (i < n):  
+            # Create random heights on average every `smoothness` steps apart
                 solution[i] = self.random.uniform(0, 100)
-                i += 1+self.random.randrange(2*smoothness)
-                
-            # Unless last value is already specified, close the circle    
-            if (pd.isna(solution[n-1])):
-                solution[n] = solution[0]
-            # Interpolate between specified points        
-            solution = solution.interpolate().tolist()    
-            if (len(solution) > n):
-                solution.pop()
-                
-            self.solution = solution              
+                i += 1 + self.random.randrange(2 * smoothness)
 
-    def tournament_step(self) -> None:
+            # Unless last value is already specified, close the circle
+            if pd.isna(solution[n - 1]):
+                solution[n] = solution[0]
+            # Interpolate between specified points
+            solution = solution.interpolate().tolist()
+            if len(solution) > n:
+                solution.pop()
+
+            self.solution = solution
+
+    def __tournament_step(self) -> None:
         """
-        Searches the landscape in tournament mode, starting from each position. Each agent searches individually for the best location they can reach, 
-        then teams move to the best location identified by team members. In the end, the best_solution attribute is updated with the average of the solutions 
+        Searches the landscape in tournament mode, starting from each position. Each agent searches individually for the best location they can reach,
+        then teams move to the best location identified by team members. In the end, the best_solution attribute is updated with the average of the solutions
         found by each team across the starting positions.
-        """        
+        """
         solutions = list()
         for i in range(self.n):
             self.current_position = dict.fromkeys(self.current_position, i)
             self.best_solution = dict.fromkeys(self.best_solution, self.solution[i])
-            while True: #Until the solution no longer improves on a full pass through the agents
+            while (True):  
+            # Until the solution no longer improves on a full pass through the agents
                 old_solution = self.best_solution
-                self.schedule.step() # Have each agent move to max position they can reach
+                self.schedule.step()  # Have each agent move to max position they can reach
                 # Update current position of each team based on maximum of agent positions
-                pos = [{"team": a.__getattribute__("team"), "solution": a.__getattribute__("best_solution"), "focus": a.__getattribute__("focus")} for a in self.schedule.agents]
+                pos = [
+                    {
+                        "team": a.__getattribute__("team"),
+                        "solution": a.__getattribute__("best_solution"),
+                        "focus": a.__getattribute__("focus"),
+                    }
+                    for a in self.schedule.agents
+                ]
                 teams = set(d["team"] for d in pos)
-                self.current_position = {t:max(list(filter(lambda d: d['team'] == t, pos)), key = lambda x:x["solution"])["focus"] for t in teams} 
-                self.best_solution = {t:max(list(filter(lambda d: d['team'] == t, pos)), key = lambda x:x["solution"])["solution"] for t in teams}
+                self.current_position = {
+                    t: max(
+                        list(filter(lambda d: d["team"] == t, pos)),
+                        key=lambda x: x["solution"],
+                    )["focus"]
+                    for t in teams
+                }
+                self.best_solution = {
+                    t: max(
+                        list(filter(lambda d: d["team"] == t, pos)),
+                        key=lambda x: x["solution"],
+                    )["solution"]
+                    for t in teams
+                }
                 if old_solution == self.best_solution:
                     solutions.append(copy(self.best_solution))
                     break
-        self.running = False  
-        self.best_solution = self.dict_mean(solutions)
+        self.running = False
+        self.best_solution = self.__dict_mean(solutions)
 
     def step(self) -> None:
-        """Have agent teams search for solution, following specified strategy/strategies 
-        
+        """Have agent teams search for solution, following specified strategy/strategies
+
         This runs the simulation, going through each starting point in the landscape and getting agent teams to search for the best solution they can achieve, using either the relay or tournament strategy - or both sequentially. At the end, the best_solution attribute is updated with the average performance of each team (under each strategy, if both are simulated).
 
-        """        
-        if(self.strategy == "both"):
+        """
+        if self.strategy == "both":
             self.strategy = "relay"
             super().step()
             sol = {"relay_" + str(key): val for key, val in self.best_solution.items()}
             self.strategy = "tournament"
-            self.tournament_step()
-            self.best_solution = dict(sol, **{"tournament_" + str(key): val for key, val in self.best_solution.items()})
+            self.__tournament_step()
+            self.best_solution = dict(
+                sol,
+                **{
+                    "tournament_" + str(key): val
+                    for key, val in self.best_solution.items()
+                }
+            )
             self.strategy = "both"
             return None
 
         if self.strategy == "relay":
             super().step()
         if self.strategy == "tournament":
-            self.tournament_step()
-            self.running = False  
-
+            self.__tournament_step()
+            self.running = False
+            
--- Hong_and_Page/HPmodel.py
+++ Hong_and_Page/HPmodel.py
@@ -5,7 +5,8 @@ from collections import Counter
 from itertools import permutations
 from mesa import Agent, Model
 from mesa.time import BaseScheduler
-import copy
+from copy import copy
+
 
 class PSAgent(Agent):
 
@@ -17,11 +18,22 @@ class PSAgent(Agent):
         heuristic: List of step lengths this agent can consider.
         team: Team this agent belongs to. Either "random" or "best" in standard HPProblem.
         best_solution: Best solution found by this agent so far.
+
+    Methods:
+        step: Advance by one step - called by mesa.
     """
-    
-    def __init__(self, problem: 'HPProblem', id: str, team: str, k: int = None, l: int = None, heuristic: list = None):
+
+    def __init__(
+        self,
+        problem: "HPProblem",
+        id: str,
+        team: str,
+        k: int = None,
+        l: int = None,
+        heuristic: list = None,
+    ):
         """Initializes agent, assigns heuristic and team
-        
+
         This initializes an agent to solve a given problem, assigns them a heuristic and a team. The heuristic can
         either be given as a list of step lengths, or by specifying k (number of different step lengths) and l (maximum step length).
 
@@ -32,25 +44,26 @@ class PSAgent(Agent):
             k (int): Number of different step lengths to consider (e.g., 3 per agent)
             l (int): Maximum step length to consider (e.g., 10)
             heuristic (list): Alternative to k and l, the specific heuristic can be given
-        """        
+        """
         super().__init__(id, problem)
 
-        if heuristic == None:
-            self.draw_heuristic(k, l)
+        if heuristic is None:
+            self.__draw_heuristic(k, l)
         else:
-            self.heuristic = heuristic    
+            self.heuristic = heuristic
 
         self.best_solution = 0
         self.problem = problem
         self.team = team
 
-    def draw_heuristic(self, problem: 'HPProblem', k: int, l: int):
-        """Draw heuristic as k random integers up to l"""        
-        self.heuristic = problem.random.sample(range(1,l+1), k)
+    def __draw_heuristic(self, problem: "HPProblem", k: int, l: int):
+        """Draw heuristic as k random integers up to l"""
+        self.heuristic = problem.random.sample(range(1, l + 1), k)
 
     def step(self):
-        """Search for highest peak accessible with own heuristic (called by mesa)"""        
-        self.focus, self.best_solution = self.problem.max_search(agent = self)
+        """Search for highest peak accessible with own heuristic (called by mesa)"""
+        self.focus, self.best_solution = self.problem.max_search(agent=self)
+
 
 class HPProblem(Model):
 
@@ -67,13 +80,25 @@ class HPProblem(Model):
 
     Methods:
         max_search: Evaluate a heuristic across all starting points, or have an agent search from their current location.
-        step: Advance model by one step.    
+        draw_agents: Generate teams of agents (random and best)
+        generate_heuristics: Create heuristics (set of step sizes to be considered)
+        evaluate_heuristics: Calculate average score achieved by a given heuristic
+        assess_hp_diversity: Calculate diversity between two heuristics as defined by Hong & Page
+        step: Advance model by one step.
     """
 
-    def __init__(self, n: int, k: int, l: int, N_agents: int, seed: int = None, agent_class: Agent = PSAgent): 
+    def __init__(
+        self,
+        n: int,
+        k: int,
+        l: int,
+        N_agents: int,
+        seed: int = None,
+        agent_class: Agent = PSAgent,
+    ):
         """Initializes problem, assesses heuristics and creates agent teams
-        
-        Initializes Hong & Page model by drawing the solution landscape, assessing the performance of all heuristics, 
+
+        Initializes Hong & Page model by drawing the solution landscape, assessing the performance of all heuristics,
         and creating best and random agent teams.
 
         Args:
@@ -82,12 +107,12 @@ class HPProblem(Model):
             l: Maximum step size to be considered when drawing heuristics
             N_agents: Number of agents in each team
             seed: Random seed for reproducibility
-        """        
-        #Seed automatically set by mesa if provided
+        """
+        # Seed automatically set by mesa if provided
         self.schedule = BaseScheduler(self)
         self.agent_descriptives = {}
         self.n = n
-        self.draw_solution(n)
+        self.__draw_solution(n)
         self.optimal_solution = max(self.solution)
         self.best_solution = {"random": 0, "best": 0}
         self.current_position = {"random": 0, "best": 0}
@@ -96,7 +121,7 @@ class HPProblem(Model):
 
     def draw_agents(self, k: int, l: int, N_agents: int, agent_class: Agent) -> None:
         """Generates both random and best agent teams
-        
+
         To generate the team of 'best' agents, all possible heuristics are evaluated across all starting points.
         Then the N_agents best-performing heuristics are used to create the team.
 
@@ -114,8 +139,8 @@ class HPProblem(Model):
         descriptives = {
             "worst_agent": min(heuristics.values()),
             "average_agent": mean(heuristics.values()),
-            "top_agent": max(heuristics.values())        
-            }
+            "top_agent": max(heuristics.values()),
+        }
 
         # Draw "random" team based on randomly selected heuristics, and best team based on highest-performing heuristics
         for team_type in ["random", "best"]:
@@ -125,106 +150,121 @@ class HPProblem(Model):
                 heuristics_selected = dict(Counter(heuristics).most_common(N_agents))
             descriptives["team_average"] = mean(heuristics_selected.values())
             pairs = permutations(heuristics_selected, 2)
-            descriptives["NPdiversity"] = mean([self.assess_hp_diversity(x[0], x[1]) for x in pairs])
-            
+            descriptives["NPdiversity"] = mean(
+                [self.assess_hp_diversity(x[0], x[1]) for x in pairs]
+            )
+
             # Initialise agents and add them to the scheduler
 
-            agents = [agent_class(self, id = team_type + str(idx), team = team_type, heuristic=val) for idx, val in enumerate(heuristics_selected)]
+            agents = [
+                agent_class(
+                    self, id=team_type + str(idx), team=team_type, heuristic=val
+                )
+                for idx, val in enumerate(heuristics_selected)
+            ]
 
             for agent in agents:
                 self.schedule.add(agent)
-    
-            self.agent_descriptives[team_type] = copy.copy(descriptives)
 
-    def draw_solution(self, n: int) -> None:
-        """Generate solution landscape: n random numbers up to 100"""        
+            self.agent_descriptives[team_type] = copy(descriptives)
+
+    def __draw_solution(self, n: int) -> None:
+        """Generate solution landscape: n random numbers up to 100"""
         self.solution = [self.random.uniform(0, 100) for i in range(n)]
 
-    def max_search(self, agent: Agent = None, heuristic: list = None, update: bool = True) -> tuple:
+    def max_search(
+        self, agent: Agent = None, heuristic: list = None, update: bool = True
+    ) -> tuple:
         """Either evaluates a heuristic across all starting points, or has an agent search from their current location
-        
+
         If an agent is provided, the agent will search from their current location, trying out each step size in their heuristic in turn until
         they cannot reach a higher value. If a heuristic is provided, the heuristic will be evaluated across all starting points. `update` determines
         whether the team's current position (and best_solution) should be updated.
 
-        Returns: A tuple (best_solution, current_position) that represents the best solution found and the current position of the agent 
+        Returns: A tuple (best_solution, current_position) that represents the best solution found and the current position of the agent
           (NB: when a heuristic is evaluated, the latter is meaningless).
-        """        
-        N = self.n #To speed things up
+        """
+        N = self.n  # To speed things up
         SOLUTION = self.solution
 
-        if heuristic == None: #When agents search, start from their current location
+        if heuristic is None:  # When agents search, start from their current location
             start = [self.current_position[agent.team]]
             heuristic = agent.heuristic
-        else: #When heuristics are evaluated, search should start from each location in turn
+        else:  # When heuristics are evaluated, search should start from each location in turn
             start = range(N)
 
         optima = []
 
         # Search for highest peak accessible with heuristic
         for current in start:
-            last_value = SOLUTION[current % N] #Turn landscape into a ring
+            last_value = SOLUTION[current % N]  # Turn landscape into a ring
 
-            while True: # Take steps using heuristic unless there are no further improvements
+            while (True):  
+            # Take steps using heuristic unless there are no further improvements
                 old_value = last_value
                 for step in heuristic:
-                    new_value = SOLUTION[(current+step) % N]
-                    if  new_value > last_value:
+                    new_value = SOLUTION[(current + step) % N]
+                    if new_value > last_value:
                         last_value = new_value
                         current += step
-                if old_value == last_value: #No change on k checks
+                if old_value == last_value:  # No change on k checks
                     optima.append(last_value)
-                    break 
+                    break
 
-        if update: #Should only be used when agents search
+        if update:  # Should only be used when agents search
             self.best_solution[agent.team] = optima[0]
-            self.current_position[agent.team] = current    
+            self.current_position[agent.team] = current
 
-        return current, mean(optima) 
+        return current, mean(optima)
 
-    def generate_heuristics(self, k: int,l: int) -> list:
-        """Generates all possible heuristics"""        
+    def generate_heuristics(self, k: int, l: int) -> list:
+        """Generates all possible heuristics"""
         return permutations(range(1, l + 1), k)
 
     def evaluate_heuristics(self, heuristics: list) -> dict:
         """Calculates 'ability' score for each heuristic - the mean result from each starting point
-        
+
         Returns: Dict with heuristics as keys and their average score as value
-        """        
+        """
         expectations = {}
         for heuristic in heuristics:
-            expectations[heuristic] = self.max_search(heuristic = heuristic, update=False)[1]
-        return expectations     
+            expectations[heuristic] = self.max_search(
+                heuristic=heuristic, update=False
+            )[1]
+        return expectations
 
-    def __sample_from_dict(self, d: dict, n: int): #From https://stackoverflow.com/a/66018057/10581449
-        """Return n random objects from d"""        
+    def __sample_from_dict(self, d: dict, n: int):  
+        # From https://stackoverflow.com/a/66018057/10581449
+        """Return n random objects from d"""
         keys = self.random.sample(list(d), n)
         values = [d[k] for k in keys]
         return dict(zip(keys, values))
 
     def assess_hp_diversity(self, heuristic1: list, heuristic2: list) -> float:
         """Calculates diversity between two heuristics as defined by Hong & Page
-        
-        Diversity here is defined as the share of positions in which the two heuristics differ. 
+
+        Diversity here is defined as the share of positions in which the two heuristics differ.
         For instance (1,2,3) and (1,3,4) would have a diversity of 67% since they overlap in the first position only.
-        """        
-        res = (len(heuristic1)-sum(x == y for x, y in zip(heuristic1, heuristic2)))/len(heuristic1)
+        """
+        res = (
+            len(heuristic1) - sum(x == y for x, y in zip(heuristic1, heuristic2))
+        ) / len(heuristic1)
         return res
 
-    def dict_mean(self, dict_list: list) -> dict:
-        """Calculates the mean of values in a list of dictionaries, grouped by each key"""        
-        #Thanks to https://stackoverflow.com/a/55220333/10581449
+    def __dict_mean(self, dict_list: list) -> dict:
+        """Calculates the mean of values in a list of dictionaries, grouped by each key"""
+        # Thanks to https://stackoverflow.com/a/55220333/10581449
         mean_dict = {}
         for key in dict_list[0].keys():
             mean_dict[key] = sum(d[key] for d in dict_list) / len(dict_list)
         return mean_dict
 
     def step(self) -> None:
-        """Has agent teams search for solution 
-        
+        """Has agent teams search for solution
+
         This runs the simulation, going through each starting point in the landscape and getting agent teams to search for the best solution they can achieve.
         At the end, the best_solution attribute is updated with the average performance of each team.
-        """        
+        """
         solutions = list()
         for i in range(self.n):
             self.current_position = dict.fromkeys(self.current_position, i)
@@ -232,8 +272,7 @@ class HPProblem(Model):
                 old_solution = self.best_solution
                 self.schedule.step()
                 if old_solution == self.best_solution:
-                    solutions.append(copy.copy(self.best_solution))
+                    solutions.append(copy(self.best_solution))
                     break
-        self.best_solution = self.dict_mean(solutions)
-        self.running = False    
-
+        self.best_solution = self.__dict_mean(solutions)
+        self.running = False
